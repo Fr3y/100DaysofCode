a3dd9f70e67926a40dd69356660e7a6226e66d49
Frey <free3y@gmail.com>
2021-07-15 22:26:38 +0530

    Create 7.c

---
+++ 7.c
@@ -0,0 +1,25 @@
+// yeah i'm on streak 
+// arithmetic fun
+#include <stdio.h>
+int main() {
+    int len = 10;
+    int w = 7;
+    int a;
+    int s = 90;
+    int bot = 4;
+    int you;
+    int me;
+    float money = 100000;
+    float haha = 5;
+    a = len*w;
+    you = bot/money;
+    me = haha / s;
+    printf("%d\n", a);
+    printf("%d\n", you);
+    printf("%d\n", me);
+    return 0;
+}
+// important thing here is that / ------> divide 
+// % ------> reminder 
+
+
